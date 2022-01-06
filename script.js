981890b2dc16b6c42f98a4d9943dd076d397a399
aishaRasifa <aisharasifa@gmail.com>
2022-01-07 01:01:17 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-// this is script.js git test
+ // this is script.js git test
 // test commit by jaifer
 // test commit by aisha
+var _ = require('lodash');
+var array =[1,2,3,4,5,6,7,8];
+console.log('answer:',_.without(array,3));
